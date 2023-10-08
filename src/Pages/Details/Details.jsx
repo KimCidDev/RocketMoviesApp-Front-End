c4e73384095259819ee3a3d33198935758d8dce1
Kim <cid.kim@gmail.com>
2023-10-07 21:27:44 -0600

    Details page ready

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,18 +1,43 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
 
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details () {
 
   return (
     <Container>
-      <h1>Something important</h1>
-      <span>something else</span>
+      <Header />
 
-      <Button  title="login" loading/>
-      <Button title="logout" />
-      <Button  title="signup" />
-      <Button  title="Felipe Ávila" loading/>
+      <main>
+        <Content>
+
+      <ButtonText title="Excluir Nota"/>
+
+      <h1> Introdução ao React</h1>
+      <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit pariatur quis consequuntur, quo eaque suscipit ipsa quod asperiores quaerat officia voluptatem ad excepturi nesciunt debitis beatae cumque optio cum facilis! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit pariatur quis consequuntur, quo eaque suscipit ipsa quod asperiores quaerat officia voluptatem ad excepturi nesciunt debitis beatae cumque optio cum facilis!
+       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit pariatur quis consequuntur, quo eaque suscipit ipsa quod asperiores quaerat officia voluptatem ad excepturi nesciunt debitis beatae cumque optio cum facilis!</p>
+      <Section title="Links Úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="nodejs"/>
+        <Tag title="express"/>
+      </Section>
+
+      <Button  title="Voltar"/>
+
+                
+      </Content>
+      </main>
+
     </Container>
   )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles';
+
+export function Header () {
+
+  return (
+  <Container >
+    <Profile>
+      <img src="http://github.com/kimciddev.png" alt="User's profile image" />
+
+      <div>
+        <span>
+          Bem-Vindo
+        </span>
+        <strong>
+          Kim Cid Teixeira
+        </strong>
+      </div>
+    </Profile>
+
+       <Logout>
+        <RiShutDownLine />
+      </Logout>
+
+  </ Container>
+  )
+}
