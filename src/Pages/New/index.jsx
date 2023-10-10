035de1f8a3825e33ff9077b3dd26c1baa0bb451f
Kim <cid.kim@gmail.com>
2023-10-10 14:45:47 -0600

    New (note) Page done

--- src/Pages/New/index.jsx
+++ src/Pages/New/index.jsx
@@ -3,6 +3,9 @@ import { Container, Form } from './styles';
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
 
 export function New () {
   return (
@@ -21,6 +24,21 @@ export function New () {
           placeholder="Título"
           />
           <Textarea placeholder="Observações" />
+
+          <Section title="links úteis">
+            <NoteItem value="https://google.com.br"/>
+            <NoteItem isNew placeholder="Novo Link"/>
+          </Section>
+
+
+          <Section title="Marcadores">
+            <div className="tags">
+            <NoteItem value="https://google.com.br"/>
+            <NoteItem isNew placeholder="Nova Tag"/>
+            </div>
+          </Section>
+
+          <Button title="Salvar"/>
         </Form>
       </main>
     </Container>
