44e05c7caaa59b364b8bc90dd878edefb983cc46
Kim <cid.kim@gmail.com>
2023-10-10 13:33:58 -0600

    Profile page done

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -1,6 +1,6 @@
 import { Container, Form, Avatar } from './styles';
 
-import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiLock, FiUser, FiCamera } from 'react-icons/fi';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -11,10 +11,23 @@ export function Profile () {
   return (
     <Container>
       <header>
-        <a href="/"><FiArrowLeft /></a>
+        <a href="/">
+          <FiArrowLeft />
+          </a>
       </header>
 
     <Form>
+      <Avatar>
+        <img src="http://github.com/kimciddev.png" alt="User's profile picture" />
+
+        <label htmlFor="avatar">
+          <FiCamera />
+          <input 
+          id="avatar"
+          type="file" />
+        </label>
+
+      </Avatar>
       <Input
         placeholder="Name"
         type="text"
