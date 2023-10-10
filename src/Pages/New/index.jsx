b658f455e2d8928928efd160ea1fd51ccf9f388f
Kim <cid.kim@gmail.com>
2023-10-10 14:03:16 -0600

    component textarea

--- src/Pages/New/index.jsx
+++ src/Pages/New/index.jsx
@@ -1,12 +1,28 @@
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
 
 export function New () {
   return (
     <Container>
       <Header />
 
+
+      <main>
+        <Form>
+          <header>
+            <h1>Create New Note</h1>
+            <a href="/"> Voltar</a>
+          </header>
+
+          <Input 
+          placeholder="Título"
+          />
+          <Textarea placeholder="Observações" />
+        </Form>
+      </main>
     </Container>
 
   )
