15a14613ebec9218b363b5e97acd5ee3e2d2a765
Kim <cid.kim@gmail.com>
2023-10-18 21:18:16 -0600

    signUp page backgroundImg change

--- src/Pages/SignUp/styles.js
+++ src/Pages/SignUp/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import backgroundImg from '../../assets/background.png';
+import backgroundImg from '../../assets/background3-mindot.jpg';
 
 export const Container = styled.div`
   height: 100vh;
